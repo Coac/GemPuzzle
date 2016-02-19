4d8cf57ef278e1d8b5eb682bc1559da883235a52
Quentin de Longraye <quentin@dldl.fr>
2016-02-19 11:47:08 +0100

    wip parser

--- src/element/PuzzleGrid.java
+++ src/element/PuzzleGrid.java
@@ -1,6 +1,5 @@
 package element;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PuzzleGrid<T> {
@@ -8,29 +7,27 @@ public class PuzzleGrid<T> {
 	private List<Tile<T>> tiles;
 	private int size;
 	
-	public PuzzleGrid(int size) {
+	public PuzzleGrid(int size, List<Tile<T>> tiles) {
 		this.size = size;
-		tiles = new ArrayList<Tile<T>>();
+		this.tiles = tiles;
 	}
 	
 	public Tile<T> getTile(int i, int j) {
-		return tiles.get(i%size + j*size);
+		return this.tiles.get(i%size + j*size);
 	}
 	
 	public T getElement(int i, int j) {
-		return getTile(i,j).get();
+		return this.getTile(i,j).get();
 	}
 	
-	
-	
-	private void swap(int i1, int j1, int i2, int j2) {
+	public void swap(int i1, int j1, int i2, int j2) {
 		Tile<T> temp = this.getTile(i1, j1);
 		this.setTile(i1, j1, this.getTile(i2, j2));
 		this.setTile(i2, j2, temp);
 	}
 	
 	private void setTile(int i, int j, Tile<T> newTile) {
-		tiles.set(i%size + j*size, newTile);
+		this.tiles.set(i%size + j*size, newTile);
 	}
 
 }
