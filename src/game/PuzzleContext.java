7bef21189bf1814fbd485801db51d7369f933768
Victor Le <m.victor.le@gmail.com>
2016-02-19 10:45:52 +0100

    goalGrid in PuzzleContext

--- src/game/PuzzleContext.java
+++ src/game/PuzzleContext.java
@@ -6,14 +6,27 @@ import element.Grid;
 public class PuzzleContext<T> {
 	
 	private Grid<T> grid;
+	private Grid<T> goalGrid;
 	private ArtificialIntelligenceInterface ai;
 	
-	public PuzzleContext(Grid<T> grid) {
+	public PuzzleContext(Grid<T> grid, Grid<T> goalGrid) {
 		this.grid = grid;
+		this.goalGrid = goalGrid;
 	}
 	
 	public void setAI(ArtificialIntelligenceInterface ai) {
 		this.ai = ai;
+	}
+	
+	public Grid<T> getGrid() {
+		return this.grid;
+	}
+	public Grid<T> getGoal() {
+		return this.grid;
+	}
+	
+	public boolean hasWin() {
+		return this.grid.equals(this.goalGrid);
 	}
 
 }
