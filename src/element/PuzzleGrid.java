18927828a383d9c3603a047a8520a48fc8b01d8f
Victor Le <m.victor.le@gmail.com>
2016-02-19 11:13:42 +0100

    Move

--- src/element/PuzzleGrid.java
+++ src/element/PuzzleGrid.java
@@ -3,10 +3,13 @@ package element;
 import java.util.ArrayList;
 import java.util.List;
 
+import game.Move;
+
 public class PuzzleGrid<T> {
 	
 	private List<Tile<T>> tiles;
 	private int size;
+	private int nullIndex;
 	
 	public PuzzleGrid(int size) {
 		this.size = size;
@@ -18,12 +21,31 @@ public class PuzzleGrid<T> {
 	}
 	
 	public T getElement(int i, int j) {
-		return getTile(i,j).get();
+		return getTile(i,j).getValue();
 	}
 	
+	public void setMove(Move move) {
+		switch(move.get()) {
+		case Down:
+			swapIndex(nullIndex, nullIndex + size);
+		case Left:
+			swapIndex(nullIndex, nullIndex - 1);
+		case Right:
+			swapIndex(nullIndex, nullIndex + 1);
+		case Up:
+			swapIndex(nullIndex, nullIndex - size);
+		default:
+			throw new RuntimeException("Illegal move");
+		}
+	}
 	
+	private void swapIndex(int index1, int index2) {
+		Tile<T> temp = this.tiles.get(index1);
+		this.tiles.set(index1,this.tiles.get(index2));
+		this.tiles.set(index2, temp);
+	}
 	
-	private void swap(int i1, int j1, int i2, int j2) {
+	private void swapCoord(int i1, int j1, int i2, int j2) {
 		Tile<T> temp = this.getTile(i1, j1);
 		this.setTile(i1, j1, this.getTile(i2, j2));
 		this.setTile(i2, j2, temp);
