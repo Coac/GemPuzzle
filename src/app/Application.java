66686b4747efd4e2a3604b3059e6b39606352377
Mathieu Porcel <mathieu.porcel@gmail.com>
2016-02-19 11:30:24 +0100

    Test graphique du jeu

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,10 +1,18 @@
 package app;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !");
-
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		new Window();
 	}
 
 }
---
+++ src/app/PanelGame.java
@@ -0,0 +1,52 @@
+package app;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PanelGame extends JPanel {
+	private static final int MARGIN_CASE = 5;
+
+	public PanelGame() {
+
+	}
+
+	@Override
+	public void paint(Graphics graph) {
+		Graphics2D g = (Graphics2D) graph;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		g.setColor(new Color(150, 150, 150));
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		int n = 5;
+		int sizeCase = Math.min(getWidth(), getHeight()) / n - MARGIN_CASE;
+		Font font = new Font("Default", Font.BOLD, sizeCase / 2);
+		FontMetrics metrics = g.getFontMetrics(font);
+		g.setFont(font);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				g.setColor(new Color(200, 200, 200));
+				g.fillRoundRect(2 * MARGIN_CASE + i * sizeCase, 2 * MARGIN_CASE + j * sizeCase, sizeCase - MARGIN_CASE,
+						sizeCase - MARGIN_CASE, 16, 16);
+
+				g.setColor(new Color(100, 100, 100));
+				g.drawRoundRect(2 * MARGIN_CASE + i * sizeCase, 2 * MARGIN_CASE + j * sizeCase, sizeCase - MARGIN_CASE,
+						sizeCase - MARGIN_CASE, 16, 16);
+
+				String str = (1 + i + n * j) + "";
+				int fontX = (sizeCase - metrics.stringWidth(str)) / 2;
+				int fontY = (sizeCase + metrics.getAscent() - metrics.getDescent()) / 2;
+				g.drawString(str, 2 * MARGIN_CASE + i * sizeCase + fontX, 2 * MARGIN_CASE + j * sizeCase + fontY);
+			}
+		}
+	}
+}
