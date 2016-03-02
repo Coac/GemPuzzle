1f2793b89e08e6f60724a94b52f95286752006fe
Victor Le <m.victor.le@gmail.com>
2016-03-02 19:11:50 +0100

    AbstractArtificialIntelligence now takes Grid instead of Context

--- src/game/PuzzleContext.java
+++ src/game/PuzzleContext.java
@@ -14,7 +14,7 @@ public class PuzzleContext<T> {
 	
 	public void setAI(AbstractArtificialIntelligence<T> ai) {
 		this.ai = ai;
-		this.ai.setContext(this);
+		this.ai.setGrid(this.grid);
 	}
 	
 	public PuzzleGrid<T> getGrid() {
