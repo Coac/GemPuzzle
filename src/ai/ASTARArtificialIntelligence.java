a9eb74b79ffecde4b2448311f63b27353fee3597
BokuWaToriTori <dimitri.abbey@ensimag.grenoble-inp.fr>
2016-03-03 19:14:25 +0100

    warnings

--- src/ai/ASTARArtificialIntelligence.java
+++ src/ai/ASTARArtificialIntelligence.java
@@ -1,10 +1,6 @@
 package ai;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 import element.PuzzleGrid;
 import game.Move;
 import utils.HashMapWithCounters;
--- src/ai/DFSArtificialIntelligence.java
+++ src/ai/DFSArtificialIntelligence.java
@@ -1,13 +1,8 @@
 package ai;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 import element.PuzzleGrid;
-import game.History;
 import game.Move;
 import utils.HashMapWithCounters;
 import utils.ListWithCounters;
