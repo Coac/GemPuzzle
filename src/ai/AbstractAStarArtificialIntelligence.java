40cd46e4890a99b239ee5f5c07236c49ec6b3380
Quentin de Longraye <quentin@dldl.fr>
2016-03-04 12:28:40 +0100

    Revert "Optimal solution with max cost"
    This reverts commit 19558fd6e1cf1c6e9ac35cffedd91e0188420698.

--- src/ai/AbstractAStarArtificialIntelligence.java
+++ src/ai/AbstractAStarArtificialIntelligence.java
@@ -9,69 +9,59 @@ import utils.ListWithCounters;
 import utils.Pair;
 
 public abstract class AbstractAStarArtificialIntelligence<T> extends AbstractArtificialIntelligence<T> {
-	private int maxCost = 99999999;
-
+	
 	@Override
 	public void silentSolve() {
 		HashMapWithCounters<GridState<T>, GridState<T>> parent = new HashMapWithCounters<GridState<T>, GridState<T>>();
-		ListWithCounters<GridState<T>> gridStateQueue = new ListWithCounters<GridState<T>>();
+		ListWithCounters<GridState<T>> gridStateQueue = new ListWithCounters<GridState<T>>();	  	
 		GridState<T> currentState = new GridState<T>(this.grid, 0, null);
 		gridStateQueue.add(currentState);
 
-		GridState<T> polledGridState = gridStateQueue.poll();
-		while (polledGridState != null) {
+  		GridState<T> polledGridState = gridStateQueue.poll();
+	  	while(!polledGridState.getGrid().isSolved()) {
+ 		  	List<Pair<PuzzleGrid<T>, Move>> adjacentsPuzzle = polledGridState.getGrid().getAdjacentPuzzles();
+  			
+	  		for(Pair<PuzzleGrid<T>, Move> adjPuzzle : adjacentsPuzzle) {
+	  			int adjacentCost = polledGridState.getCost() + 1 + this.getHeuristic(adjPuzzle.getFirst());
+	  			GridState<T> adjacentState = new GridState<T>(adjPuzzle.getFirst(), adjacentCost, adjPuzzle.getSecond());
 
-			if (polledGridState.getGrid().isSolved()) {
-				maxCost = polledGridState.getCost();
-				GridState<T> previous = polledGridState;
-				while (parent.containsKey(previous)) {
-					this.history.addHead(previous.getMove());
-					previous = parent.get(previous);
-				}
+  				GridState<T> existingState = parent.get(adjacentState);
+	  			if(existingState != null && adjacentCost < existingState.getCost()) {
+  					existingState.refreshCost(adjacentCost);
+  					existingState.refreshMove(adjPuzzle.getSecond());
+	  			} else {
+	  				existingState = adjacentState;
+	  				parent.put(existingState, polledGridState);
+	  			}
+	  			
+	  			
+	  			int index = gridStateQueue.indexOf(existingState);
+	  			if(index >= 0) {
+	  				GridState<T> state = gridStateQueue.get(index);
+	  				if (state.getCost() > existingState.getCost()) {
+	  					state.refreshCost(existingState.getCost());
+	  					Collections.sort(gridStateQueue);
+	  				}
+	  			}
+	  			else {
+				gridStateQueue.add(existingState);
+				Collections.sort(gridStateQueue);
+	  			}
 
-				this.stats.add(parent.getStatistics());
-				this.stats.add(gridStateQueue.getStatistics());
+	  		}
 
-			}
+	  		++this.iterationsNumber;
+	  		polledGridState = gridStateQueue.poll();
+	  	}	
 
-			if (polledGridState.getCost() < maxCost) {
-
-				List<Pair<PuzzleGrid<T>, Move>> adjacentsPuzzle = polledGridState.getGrid().getAdjacentPuzzles();
-
-				for (Pair<PuzzleGrid<T>, Move> adjPuzzle : adjacentsPuzzle) {
-					int adjacentCost = polledGridState.getCost() + 1 + this.getHeuristic(adjPuzzle.getFirst());
-					GridState<T> adjacentState = new GridState<T>(adjPuzzle.getFirst(), adjacentCost,
-							adjPuzzle.getSecond());
-
-					GridState<T> existingState = parent.get(adjacentState);
-					if (existingState != null && adjacentCost < existingState.getCost()) {
-						existingState.refreshCost(adjacentCost);
-						existingState.refreshMove(adjPuzzle.getSecond());
-					} else {
-						existingState = adjacentState;
-						parent.put(existingState, polledGridState);
-					}
-
-					int index = gridStateQueue.indexOf(existingState);
-					if (index >= 0) {
-						GridState<T> state = gridStateQueue.get(index);
-						if (state.getCost() > existingState.getCost()) {
-							state.refreshCost(existingState.getCost());
-							Collections.sort(gridStateQueue);
-						}
-					} else {
-						gridStateQueue.add(existingState);
-						Collections.sort(gridStateQueue);
-					}
-
-				}
-			}
-
-			++this.iterationsNumber;
-			polledGridState = gridStateQueue.poll();
+		GridState<T> previous = polledGridState;
+		while (parent.containsKey(previous)) {
+			this.history.addHead(previous.getMove());
+			previous = parent.get(previous);
 		}
-
+		this.stats.add(parent.getStatistics());
+		this.stats.add(gridStateQueue.getStatistics());
 	}
-
+	
 	public abstract int getHeuristic(PuzzleGrid<T> puzzle);
 }
