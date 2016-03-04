3fa28e6af85b2d51de33ed4b818578520e6e4d86
Victor Le <m.victor.le@gmail.com>
2016-03-04 12:33:06 +0100

    Indentation A* Abstract

--- src/ai/AbstractAStarArtificialIntelligence.java
+++ src/ai/AbstractAStarArtificialIntelligence.java
@@ -9,50 +9,49 @@ import utils.ListWithCounters;
 import utils.Pair;
 
 public abstract class AbstractAStarArtificialIntelligence<T> extends AbstractArtificialIntelligence<T> {
-	
+
 	@Override
 	public void silentSolve() {
 		HashMapWithCounters<GridState<T>, GridState<T>> parent = new HashMapWithCounters<GridState<T>, GridState<T>>();
-		ListWithCounters<GridState<T>> gridStateQueue = new ListWithCounters<GridState<T>>();	  	
+		ListWithCounters<GridState<T>> gridStateQueue = new ListWithCounters<GridState<T>>();
 		GridState<T> currentState = new GridState<T>(this.grid, 0, null);
 		gridStateQueue.add(currentState);
 
-  		GridState<T> polledGridState = gridStateQueue.poll();
-	  	while(!polledGridState.getGrid().isSolved()) {
- 		  	List<Pair<PuzzleGrid<T>, Move>> adjacentsPuzzle = polledGridState.getGrid().getAdjacentPuzzles();
-  			
-	  		for(Pair<PuzzleGrid<T>, Move> adjPuzzle : adjacentsPuzzle) {
-	  			int adjacentCost = polledGridState.getCost() + 1 + this.getHeuristic(adjPuzzle.getFirst());
-	  			GridState<T> adjacentState = new GridState<T>(adjPuzzle.getFirst(), adjacentCost, adjPuzzle.getSecond());
+		GridState<T> polledGridState = gridStateQueue.poll();
+		while (!polledGridState.getGrid().isSolved()) {
+			List<Pair<PuzzleGrid<T>, Move>> adjacentsPuzzle = polledGridState.getGrid().getAdjacentPuzzles();
 
-  				GridState<T> existingState = parent.get(adjacentState);
-	  			if(existingState != null && adjacentCost < existingState.getCost()) {
-  					existingState.refreshCost(adjacentCost);
-  					existingState.refreshMove(adjPuzzle.getSecond());
-	  			} else {
-	  				existingState = adjacentState;
-	  				parent.put(existingState, polledGridState);
-	  			}
-	  			
-	  			
-	  			int index = gridStateQueue.indexOf(existingState);
-	  			if(index >= 0) {
-	  				GridState<T> state = gridStateQueue.get(index);
-	  				if (state.getCost() > existingState.getCost()) {
-	  					state.refreshCost(existingState.getCost());
-	  					Collections.sort(gridStateQueue);
-	  				}
-	  			}
-	  			else {
-				gridStateQueue.add(existingState);
-				Collections.sort(gridStateQueue);
-	  			}
+			for (Pair<PuzzleGrid<T>, Move> adjPuzzle : adjacentsPuzzle) {
+				int adjacentCost = polledGridState.getCost() + 1 + this.getHeuristic(adjPuzzle.getFirst());
+				GridState<T> adjacentState = new GridState<T>(adjPuzzle.getFirst(), adjacentCost,
+						adjPuzzle.getSecond());
 
-	  		}
+				GridState<T> existingState = parent.get(adjacentState);
+				if (existingState != null && adjacentCost < existingState.getCost()) {
+					existingState.refreshCost(adjacentCost);
+					existingState.refreshMove(adjPuzzle.getSecond());
+				} else {
+					existingState = adjacentState;
+					parent.put(existingState, polledGridState);
+				}
 
-	  		++this.iterationsNumber;
-	  		polledGridState = gridStateQueue.poll();
-	  	}	
+				int index = gridStateQueue.indexOf(existingState);
+				if (index >= 0) {
+					GridState<T> state = gridStateQueue.get(index);
+					if (state.getCost() > existingState.getCost()) {
+						state.refreshCost(existingState.getCost());
+						Collections.sort(gridStateQueue);
+					}
+				} else {
+					gridStateQueue.add(existingState);
+					Collections.sort(gridStateQueue);
+				}
+
+			}
+
+			++this.iterationsNumber;
+			polledGridState = gridStateQueue.poll();
+		}
 
 		GridState<T> previous = polledGridState;
 		while (parent.containsKey(previous)) {
@@ -62,6 +61,6 @@ public abstract class AbstractAStarArtificialIntelligence<T> extends AbstractArt
 		this.stats.add(parent.getStatistics());
 		this.stats.add(gridStateQueue.getStatistics());
 	}
-	
+
 	public abstract int getHeuristic(PuzzleGrid<T> puzzle);
 }
